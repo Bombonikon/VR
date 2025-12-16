63dd5f3efa9e4ee6d4b996dcd4354f071a07f7a1
Bombonikon <makowskidawid111@gmail.com>
2025-12-16 22:16:13 +0100

    Delete core directory

--- core/DebugStart.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-@export var delay := 1.0
-
-func _ready():
-	await get_tree().create_timer(delay).timeout
-	get_node("/root/Main/GameManager").start_game()
--- core/GameLoader.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-@export var gameplay_scene : PackedScene
-
-func start_game():
-	GameManager.set_state(GameManager.State.GENERATING)
-	get_tree().change_scene_to_packed(gameplay_scene)
